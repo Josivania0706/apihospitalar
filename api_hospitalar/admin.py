987543034c9e2886940dd1dfce83f103257524d3
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-08-22 17:01:06 -0400

    update
    update no banco de dados.

--- api_hospitalar/admin.py
+++ api_hospitalar/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from api_hospitalar.models import Medico, Paciente, Consulta
 # Register your models here.
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'especializacao', 'crm']
+    ordering = ['nome',]
+
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'idade', 'cidade']
+    ordering = ['nome',]
+
+class ConsultaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'paciente', 'medico', 'data_consulta']
+    ordering = ['data_consulta',]
+
+# register in django admin
+admin.site.register(Medico, MedicoAdmin)
+admin.site.register(Paciente, PacienteAdmin)
+admin.site.register(Consulta, ConsultaAdmin)
