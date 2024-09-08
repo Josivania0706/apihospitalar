8ca6f1e5a235812757d4bd85253eb849d3481751
Josivania0706 <61471213+Josivania0706@users.noreply.github.com>
2024-09-08 17:40:42 -0300

    A

--- core_api/settings.py
+++ core_api/settings.py
@@ -87,7 +87,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'apihospitalar@gmail.com'
-EMAIL_HOST_PASSWORD = "12345"
+EMAIL_HOST_PASSWORD =  os.getenv("12345")
 AUTH_PASSWORD_VALIDATORS = [ ]
 
 LANGUAGE_CODE = 'pt-br'
